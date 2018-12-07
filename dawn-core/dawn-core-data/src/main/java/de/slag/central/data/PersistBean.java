2ca788b4ff27ad061b1fd2aa6695fbc66553fdfd
sivas <sivas@enceladus.fritz.box>
2018-12-07 14:45:24 +0100

    init persist bean

---
+++ dawn-core/dawn-core-data/src/main/java/de/slag/central/data/PersistBean.java
@@ -0,0 +1,84 @@
+package de.slag.central.data;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.MappedSuperclass;
+
+import de.slag.base.features.SlagConstants;
+import de.slag.base.tools.DateUtils;
+
+@MappedSuperclass
+public abstract class PersistBean {
+
+	private static final String ELSE_ATTRIBUTE_SEPARATOR = ";";
+
+	private static final String ELSE_ATTRIBUTE_KV_SEPARATOR = "=";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	@Column
+	private Date validUntil = DateUtils.toDate(SlagConstants.END_OF_DEKAMILLENIAL);
+
+	@Column
+	private Date createdAt = new Date();
+
+	@Column
+	private Date lastUpdate;
+
+	@Column
+	@Lob
+	private String elseAttributes;
+
+	public Long getId() {
+		return id;
+	}
+
+	protected void setElseLong(String key, Long value) {
+		setElseAttribute(key, String.valueOf(value));
+	}
+
+	protected void setElseString(String key, String value) {
+		setElseAttribute(key, value);
+	}
+
+	protected String getElseString(String key) {
+		return getElseAttribute(key, (s) -> s);
+	}
+
+	protected Long getElseLong(String key) {
+		return getElseAttribute(key, (s) -> Long.valueOf(s));
+	}
+
+	protected <T> T getElseAttribute(String key, Function<String, T> function) {
+		String elseAttribute = getElseAttribute(key);
+		if (elseAttribute == null) {
+			return null;
+		}
+		return function.apply(elseAttribute);
+	}
+
+	private void setElseAttribute(String key, String value) {
+		synchronized (this) {
+			elseAttributes = PersistBeanUtils.setElseAttribute(elseAttributes, key, value, ELSE_ATTRIBUTE_SEPARATOR,
+					ELSE_ATTRIBUTE_KV_SEPARATOR);
+		}
+	}
+
+	private String getElseAttribute(String key) {
+		return PersistBeanUtils.getElseAttribute(key, elseAttributes, ELSE_ATTRIBUTE_SEPARATOR,
+				ELSE_ATTRIBUTE_KV_SEPARATOR);
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+}
---
+++ dawn-core/dawn-core-data/src/main/java/de/slag/central/data/PersistBeanUtils.java
@@ -0,0 +1,63 @@
+package de.slag.central.data;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class PersistBeanUtils {
+
+	public static String setElseAttribute(String elseAttributes, String key, String value, String sep, String kvSep) {
+		final String elseAttributesInternal = assertString(elseAttributes);
+
+		final List<String> asList = Arrays.asList(elseAttributesInternal.split(sep));
+		final StringBuilder sb = new StringBuilder();
+		String formerValue = null;
+		for (final String keyValue : asList) {
+			if (StringUtils.isBlank(keyValue)) {
+				continue;
+			}
+			String[] split = keyValue.split(kvSep);
+			final String attributeKey = split[0];
+			final String attributeValue = split[1];
+			if (key.equals(attributeKey)) {
+				formerValue = attributeValue;
+				sb.append(attributeKey + kvSep + value);
+			} else {
+				sb.append(attributeKey + kvSep + attributeValue);
+			}
+			sb.append(sep);
+		}
+		final boolean isFormerValue = (formerValue != null);
+		if (!isFormerValue) {
+			sb.append(key + kvSep + value);
+		}
+		return sb.toString();
+	}
+
+	public static String getElseAttribute(String key, String elseAttributes, String sep, String kvSep) {
+		final String elseAttributesInternal = assertString(elseAttributes);
+
+		final List<String> asList = Arrays.asList(elseAttributesInternal.split(sep));
+		for (final String keyValue : asList) {
+			if (StringUtils.isBlank(keyValue)) {
+				continue;
+			}
+			String[] split = keyValue.split(kvSep);
+			final String attributeKey = split[0];
+			final String attributeValue = split[1];
+			if (key.equals(attributeKey)) {
+				return attributeValue;
+			}
+		}
+		return null;
+	}
+
+	private static String assertString(String string) {
+		if (string == null) {
+			return StringUtils.EMPTY;
+		}
+		return string;
+	}
+
+}
